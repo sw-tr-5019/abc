617a34d890e5730cf7f632917c48aa396c3bdb09
sw-tr-5019 <swastiktripathi0000@gmail.com>
2026-07-15 16:08:42 +0530

    Update Schema.js

--- Schema.js
+++ Schema.js
@@ -19,27 +19,3 @@ module.exports.reviewSchema=Joi.object({
         rating:Joi.number().required().min(0).max(5),
         comment:Joi.string().required()
     }).required()
-=======
-
-const Joi = require('joi');
-module.exports.listingSchema=Joi.object({
-    listing:Joi.object({
-        title:Joi.string().required(),
-       description:Joi.string().required(),
-       image: { filename:Joi.string().required(),
-        url:Joi.string().required()    },
-       price:Joi.number().required().min(0),
-       location:Joi.string().required(),
-       country:Joi.string().required(),
-       coordinates:[Joi.number().required(),Joi.number().required()],
-       category: Joi.array()
-    .items(Joi.string().valid('rooms', 'iconic cities', 'castles', 'amazing pools', 'mountains', 'arctic', 'farms', 'camping','trending')),
- } ).required()
-});
-module.exports.reviewSchema=Joi.object({
-    review:Joi.object({
-        rating:Joi.number().required().min(0).max(5),
-        comment:Joi.string().required()
-    }).required()
->>>>>>> 1f0844ad4ba906b6c25095a97d6744a8b3954bff
-})
