0d5b62cebea7706f1f475f65ef0acd1bf8734a99
sw-tr-5019 <swastiktripathi0000@gmail.com>
2026-07-15 16:09:21 +0530

    Update cloudConfig.js

--- cloudConfig.js
+++ cloudConfig.js
@@ -14,21 +14,4 @@ const storage = new CloudinaryStorage({
     allowedFormats: ["png","jpg","jpeg"],
   },
 });
-=======
-const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
-
-cloudinary.config({
-    cloud_name:process.env.CLOUD_NAME,
-    api_key:process.env.CLOUD_API_KEY,
-    api_secret:process.env.CLOUD_API_SECRET
-});
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: 'swastik_BNB_Dev',
-    allowedFormats: ["png","jpg","jpeg"],
-  },
-});
->>>>>>> 1f0844ad4ba906b6c25095a97d6744a8b3954bff
 module.exports={cloudinary,storage}
